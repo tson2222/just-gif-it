c3184455cc5b10ddad1211c8d35e492af15d32b4
s.tapia-sontag <s.tapia-sontag@wolterskluwer.com>
2018-11-28 15:31:10 +0100

    test

--- src/main/java/schultz/dustin/io/services/ConverterService.java
+++ src/main/java/schultz/dustin/io/services/ConverterService.java
@@ -5,8 +5,13 @@ import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.springframework.stereotype.Service;
+import sun.plugin2.jvm.CircularByteBuffer;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class ConverterService {
@@ -36,5 +41,26 @@ public class ConverterService {
 
         frameGrabber.stop();
         gifEncoder.finish();
+    }
+
+    public static void test(){
+        List list = new ArrayList<>();
+        list.add(127);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        int x = (int) list.get(0);
+        System.out.println((byte)x);
+    }
+
+    public static void main(String[] args) throws IOException {
+        CircularByteBuffer buffer = new CircularByteBuffer(3);
+        byte[] b = new byte[]{1, 2, 3, 4, 11, 22, 33};
+        buffer.write(b,1);
+        System.out.println(Arrays.toString(b));
+        test();
+        Integer i = 5; //example
+        Long l = Long.valueOf(i.longValue());
+        System.out.println(l);
     }
 }
