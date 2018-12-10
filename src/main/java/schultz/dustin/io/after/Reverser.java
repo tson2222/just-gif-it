1d600898e0089e453b5bab1e5fc1fc2b5ad46cc3
s.tapia-sontag <s.tapia-sontag@wolterskluwer.com>
2018-12-10 12:14:29 +0100

    test

---
+++ src/main/java/schultz/dustin/io/after/Reverser.java
@@ -0,0 +1,19 @@
+package schultz.dustin.io.after;
+
+import java.util.Comparator;
+
+public class Reverser<T> implements Comparator<T>
+{
+    private final Comparator<T> comparator;
+
+    public Reverser(Comparator<T> comparator)
+    {
+        this.comparator = comparator;
+    }
+
+    @Override
+    public int compare(T left, T right)
+    {
+        return comparator.compare(right, left);
+    }
+}
