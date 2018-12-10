bff0cd1b0b05e9e8b8c256c9d5d8bc8355d654a4
s.tapia-sontag <s.tapia-sontag@wolterskluwer.com>
2018-12-10 12:20:45 +0100

    test2d

--- src/main/java/schultz/dustin/io/Tester.java
+++ src/main/java/schultz/dustin/io/Tester.java
@@ -2,14 +2,15 @@ package schultz.dustin.io;
 
 public class Tester {
 
-    static boolean switcher = false;
-
     public static void main(String[] args) {
-        booltest();
-        int a = 60;          int b = -60;        int c = 0;
+        int a = 60;
+        int b = -60;
+        int c = 0;
+        Integer integer = new Integer(a);
+        System.out.println("Byte Value of a: " + Integer.toString(a,16));
         digits(9223372036854775807L);
-        System.out.println("Int 6 / 10 =" + 9/10);
-        System.out.println(Integer.parseInt("AB",16));
+        System.out.println("Int 6 / 10 =" + 9 / 10);
+        System.out.println(Integer.parseInt("AB", 16));
         System.out.println("60 in binary  = " + Integer.toBinaryString(a));
         System.out.println("60 in hexadecimal  = " + Integer.toHexString(a));
         System.out.println("-60 = " + Integer.toBinaryString(b));
@@ -19,56 +20,46 @@ public class Tester {
         System.out.println("60 >> 1  = " + Integer.toBinaryString(c));
         //unsigned shift
         c = a >>> 1;
-        System.out.println("60 >>> 1 = " + Integer.toBinaryString(c) );
+        System.out.println("60 >>> 1 = " + Integer.toBinaryString(c));
 
         c = b >> 1;
-        System.out.println("-60 >> 1  = " + Integer.toBinaryString(c) );
+        System.out.println("-60 >> 1  = " + Integer.toBinaryString(c));
 
         c = b >>> 1;
         System.out.println("-60 >>> 1 = " + Integer.toBinaryString(c));
 
-        int hex = 0x14; int dezi = 20;
-        if(hex == dezi)
-        System.out.println("Hex and Deci have the same number value");
+        int hex = 0x14;
+        int dezi = 20;
+        if (hex == dezi)
+            System.out.println("Hex and Deci have the same number value");
 
-        }
+    }
 
-    public static void digits(int digits){
+    public static void digits(int digits) {
         int count = 0;
 
-        while(digits > 0)
-        {
+        while (digits > 0) {
             // num = num/10
             digits /= 10;
             System.out.println(digits);
             ++count;
         }
 
-        System.out.println("Number of digits: " + count);
+        System.out.println("Number of diggits: " + count);
 
     }
 
-    public static void digits(long digits){
+    public static void digits(long digits) {
         int count = 0;
 
-        while(digits > 0)
-        {
+        while (digits > 0) {
             // num = num/10
             digits /= 10;
             System.out.println(digits);
             ++count;
         }
 
-        System.out.println("Number of digits: " + count);
+        System.out.println("Number of dawaddigits: " + count);
 
     }
-
-
-    public static void booltest(){
-    if(switcher == true){
-        System.out.println("You should have used ==");
-    }else
-        System.out.println("You used ==!");
-    }
-
 }
